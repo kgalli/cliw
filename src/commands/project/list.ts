f149f073a267dfb96430db4570166a14f3792a6e
kgallinowski <karsten.gallinowski@zalando.de>
2019-07-30 09:53:28 +0200

    Add project:set-default command

--- src/commands/project/list.ts
+++ src/commands/project/list.ts
@@ -3,6 +3,19 @@ import {cli} from 'cli-ux'
 import BaseCommand from '../../base-command'
 import ConfigUtils from '../../config/config-utils'
 
+const sortByName = (a: any, b: any) => {
+  const nameA = a.name.toUpperCase()
+  const nameB = b.name.toUpperCase()
+  if (nameA < nameB) {
+    return -1
+  }
+  if (nameA > nameB) {
+    return 1
+  }
+
+  return 0
+}
+
 export default class ProjectList extends BaseCommand {
   static description = 'list projects'
 
@@ -13,16 +26,17 @@ export default class ProjectList extends BaseCommand {
   async run() {
     const {flags} = this.parse(ProjectList)
     const projectsConfig = ConfigUtils.projectsConfigLoad()
-
-    cli.table(projectsConfig.projects, {
+    cli.table(projectsConfig.projects.sort(sortByName), {
       name: {
         minWidth: 7,
       },
-      mainConfigLocation: {}
+      mainConfigLocation: {},
+      default: {
+        get: row => row.name === projectsConfig.default ? true : ''
+      }
     }, {
       printLine: this.log,
       ...flags, // parsed flags
     })
-
   }
 }
