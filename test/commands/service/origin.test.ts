3ce15f59ccb14890b56fea11b0383926f1e1ab9e
kgallinowski <karsten.gallinowski@zalando.de>
2019-08-21 15:37:46 +0200

    Simplify service:buildorigin to service:origin cmd

--- test/commands/service/origin.test.ts
+++ test/commands/service/origin.test.ts
@@ -2,7 +2,7 @@ import {expect, test} from '@oclif/test'
 
 import {env} from '../../helper/test-helper'
 
-describe('service:buildorigin', () => {
+describe('service:origin', () => {
   context('list', () => {
     // tslint:disable-next-line:no-multi-spaces
     const expectedOutPut =  'ServiceName BuildOrigin \napi         REGISTRY    \ndb          REGISTRY'
@@ -10,7 +10,7 @@ describe('service:buildorigin', () => {
     test
       .env(env)
       .stdout()
-      .command(['service:buildorigin:list'])
+      .command(['service:origin:list'])
       .it('lists services build origin', ctx => {
         expect(ctx.stdout).to.contain(expectedOutPut)
       })
